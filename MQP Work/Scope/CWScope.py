4d1488c72092787a919ec85b6fc28ab63b382737
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2023-12-13 20:13:40 -0500

    made changes to CW scope now that we have the hardware

--- MQP Work/Scope/CWScope.py
+++ MQP Work/Scope/CWScope.py
@@ -1,11 +1,11 @@
-import os
-import numpy as np
 import chipwhisperer as cw
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class CWScope:
 
-    def __init__(self, compiled_algorithm_path, gain=25, num_samples=5000, offset=0):
+    def __init__(self, firmware_path, gain=25, num_samples=5000, offset=0):
         # setup scope
         self.scope = cw.scope()
         self.scope.default_setup()
@@ -19,11 +19,11 @@ class CWScope:
         self.scope.offset = offset
 
         # upload encryption algorithm firmware to the board
-        cw.program_target(self.scope, cw.programmers.STM32FProgrammer, os.path.realpath(__file__) + compiled_algorithm_path)
+        cw.program_target(self.scope, cw.programmers.STM32FProgrammer, firmware_path)
 
     def capture_traces(self, num_traces, fixed_key=False, fixed_pt=False):
         # init return values
-        power_traces = np.empty(num_traces)
+        power_traces = []
 
         # configure plaintext, key generation
         ktp = cw.ktp.Basic()
@@ -39,6 +39,12 @@ class CWScope:
 
             # append arrays if trace successfully captured
             if trace:
-                power_traces[i] = trace
+                power_traces.append(trace)
 
         return power_traces
+
+
+scope = CWScope("C:\\Users\\samka\\PycharmProjects\\SCLA_API_MQP\\MQP Work\\Scope\\firmware\\simpleserial-aes-CWLITEARM.hex")
+traces = scope.capture_traces(10)
+plt.plot(traces[0].wave)
+plt.show()
