4499404f9111700335d54a62ebe327b8c1f1c751
SamKarkache <swkarkache@wpi.edu>
2023-12-15 03:48:38 -0500

    broken SNR testing implementation

--- Testing/MetricTesting.py
+++ Testing/MetricTesting.py
@@ -1,3 +1,5 @@
+import os.path
+
 import h5py
 from WPI_SCA_LIBRARY.Metrics import sbox_lut, signal_to_noise_ratio
 import numpy as np
@@ -5,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 def snr_ascad_verification():
-    with h5py.File("C:\\Users\\samka\\PycharmProjects\\SCLA_API_MQP\\ATMega8515_raw_traces.h5", "r") as file:
+    with h5py.File(os.path.dirname(__file__) + "\\ATMega8515_raw_traces.h5", "r") as file:
 
         # load in datasets
         meta_data = file["metadata"]
@@ -14,20 +16,14 @@ def snr_ascad_verification():
         # meta data indices
         plaintext_idx = 3
         key_idx = 2
-        ciphertext_idx = 1
         mask_idx = 0
-
-        # key is fixed
-        key = meta_data[0][key_idx]
-        target_byte = 3
-        correct_key_byte = key[target_byte]
 
         # compute labels
         labels = []
 
         # generate intermediate values from sbox lut
         for i in range(len(traces)):
-            labels.append(sbox_lut(correct_key_byte ^ int(meta_data[i][plaintext_idx][target_byte])) ^ meta_data[i][mask_idx][15])
+            labels.append(sbox_lut(meta_data[i][key_idx][3] ^ meta_data[i][plaintext_idx][3]) ^ meta_data[i][mask_idx][15])
 
         # find unique labels
         labelsUnique = np.unique(labels)
@@ -39,12 +35,11 @@ def snr_ascad_verification():
 
         # add traces to labels
         for index, label in enumerate(labels):
-            sortedLabels[label].append(traces[index][0:50000])
+            sortedLabels[label].append(traces[index][45400:46100])
 
         snr = signal_to_noise_ratio(sortedLabels)
 
         plt.plot(snr)
         plt.show()
-
 
 snr_ascad_verification()
