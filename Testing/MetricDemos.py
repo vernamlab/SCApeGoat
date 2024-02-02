348c12a7a36099b80c4fa1c66c8c98cc59f17b4c
SamKarkache <swkarkache@wpi.edu>
2024-02-01 19:09:50 -0500

    Metric Testing is now metric Demos

--- Testing/MetricDemos.py
+++ Testing/MetricDemos.py
@@ -9,67 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def snr_ascad_verification():
-    """
-    Computes SNR with ASCAD Traces
-    """
-
-    def organize_labels_for_testing(labels, traces_set):
-        # find unique labels
-        labelsUnique = np.unique(labels)
-
-        # initialize the dictionary
-        sorted_labels = {}
-        for i in labelsUnique:
-            sorted_labels[i] = []
-
-        # add traces to labels
-        for index, label in enumerate(labels):
-            sorted_labels[label].append(traces_set[index])  # we only want to look over this interval
-
-        return sorted_labels
-
-    with h5py.File(os.path.dirname(__file__) + "\\ATMega8515_raw_traces.h5", "r") as file:
-        # obtain data from HDF5 file
-        metadata = np.array(file['metadata'][:10000])
-        traces = file['traces'][:10000, :]
-        keys = metadata['key'][:, 2]
-        plaintexts = metadata['plaintext'][:, 2]
-        rout = metadata['masks'][:, 15]
-
-        # We will use the SNR2 and SNR3 processing for metric verification
-        snr2_labels = Sbox[keys ^ plaintexts] ^ rout
-        snr3_labels = rout
-
-        # compute SNR for each
-        organizedSN2 = organize_labels_for_testing(snr2_labels, traces)
-
-        start = time.time()
-        snr2 = signal_to_noise_ratio(organizedSN2)
-        end = time.time()
-
-        organizedSN3 = organize_labels_for_testing(snr3_labels, traces)
-
-        start2 = time.time()
-        snr3 = signal_to_noise_ratio(organizedSN3)
-        end2 = time.time()
-
-        total_time = ((end - start) + (end2 - start2)) / 2
-
-        print("SNR Execution Time on 10,000 ASCAD Traces: " + str(total_time) + " seconds")
-
-        plt.plot(snr2, label="SNR2")
-        plt.plot(snr3, label="SNR3")
-        plt.title("Signal to Noise Ratio Over Samples 45400 to 46100")
-        plt.ylabel("Amplitude")
-        plt.xlabel("Sample")
-        plt.xlim(45400, 46100)
-        plt.ylim(0, 1)
-        plt.legend()
-        plt.show()
-
-
-def validate_t_test():
+def t_test_demo():
     """
     Computes t-test and plot the t-statistics and t-max for 1000 traces of fixed and 1000 traces of random
     """
@@ -110,7 +50,7 @@ def validate_t_test():
     plt.show()
 
 
-def validate_correlation():
+def correlation_demo():
     """
     Computes correlation for a correct key guess
     """
@@ -148,7 +88,7 @@ def validate_correlation():
     plt.show()
 
 
-def validate_score_and_rank():
+def score_and_rank_demo():
     """
     Scores and ranks keys using correlation. Compares result to the actual key.
     """
@@ -234,7 +174,7 @@ def correct_key_rank_vs_num_traces():
     plt.show()
 
 
-def validate_success_rate_guessing_entropy():
+def success_rate_guessing_entropy_demo():
     """
     Computes the success rate and guessing entropy.
     """
--- Testing/MetricVerification.py
+++ Testing/MetricVerification.py
@@ -1,7 +1,82 @@
+import os.path
 
-class MetricVerification():
+from WPI_SCA_LIBRARY.Metrics import *
+from WPI_SCA_LIBRARY.CWScope import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-    def __init__(self):
-        """
-        Load in the data from the Metrisca datasets
-        """
+
+def snr_ascad_verification():
+    """
+    Computes SNR with ASCAD Traces
+    """
+
+    def organize_labels_for_testing(labels, traces_set):
+        # find unique labels
+        labelsUnique = np.unique(labels)
+
+        # initialize the dictionary
+        sorted_labels = {}
+        for i in labelsUnique:
+            sorted_labels[i] = []
+
+        # add traces to labels
+        for index, label in enumerate(labels):
+            sorted_labels[label].append(traces_set[index])  # we only want to look over this interval
+
+        return sorted_labels
+
+    with h5py.File(os.path.dirname(__file__) + "\\ExampleData\\ASCAD\\ATMega8515_raw_traces.h5", "r") as file:
+        # obtain data from HDF5 file
+        metadata = np.array(file['metadata'][:10000])
+        traces = file['traces'][:10000, :]
+        keys = metadata['key'][:, 2]
+        plaintexts = metadata['plaintext'][:, 2]
+        rout = metadata['masks'][:, 15]
+
+        # We will use the SNR2 and SNR3 processing for metric verification
+        snr2_labels = Sbox[keys ^ plaintexts] ^ rout
+        snr3_labels = rout
+
+        # compute SNR for each
+        organizedSN2 = organize_labels_for_testing(snr2_labels, traces)
+
+        start = time.time()
+        snr2 = signal_to_noise_ratio(organizedSN2)
+        end = time.time()
+
+        organizedSN3 = organize_labels_for_testing(snr3_labels, traces)
+
+        start2 = time.time()
+        snr3 = signal_to_noise_ratio(organizedSN3)
+        end2 = time.time()
+
+        total_time = ((end - start) + (end2 - start2)) / 2
+
+        print("SNR Execution Time on 10,000 ASCAD Traces: " + str(total_time) + " seconds")
+
+        plt.plot(snr2, label="SNR2")
+        plt.plot(snr3, label="SNR3")
+        plt.title("Signal to Noise Ratio Over Samples 45400 to 46100")
+        plt.ylabel("Amplitude")
+        plt.xlabel("Sample")
+        plt.xlim(45400, 46100)
+        plt.ylim(0, 1)
+        plt.legend()
+        plt.show()
+
+
+# PATH = "C:\\Users\\samka\\PycharmProjects\\MQP\\SCLA_API_MQP\\Testing\\ExampleData\\MetriSCA\\masked_sbox1\\traces\\oscilloscope_traces\\oscilloscope_traces_50k_3000_samples_fixed_positive_uint8_t.csv"
+#
+# with open(PATH) as file:
+#     reader = csv.reader(file)
+#
+#     traces = np.empty(50000, dtype=object)
+#
+#     for i, row in enumerate(reader):
+#         print("{} out of 50,000 traces loaded".format(i + 1))
+#         traces[i] = row
+#
+#     print(traces[0])
+#     plt.plot(traces[0])
+#     plt.show()
