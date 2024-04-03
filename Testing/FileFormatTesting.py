a40218d1a9af9046876dec4a1ad62f6f02c03ae1
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2024-04-03 17:18:20 -0400

    add blank Demo files for NEHWS

--- Testing/FileFormatTesting.py
+++ Testing/FileFormatTesting.py
@@ -5,14 +5,12 @@ from WPI_SCA_LIBRARY.FileFormat import *
 fixed = read_bin_file_traces("unprotected_sbox\\single\\traces\\oscilloscope_traces\\oscilloscope_traces_50k_3000_samples_fixed_positive_uint8_t.bin")
 random = read_bin_file_traces("unprotected_sbox\\single\\traces\\oscilloscope_traces\\oscilloscope_traces_50k_3000_samples_random_positive_uint8_t.bin")
 
-file = FileParent("AnotherFile-1", "C:\\Users\\samka\\PycharmProjects\\MQP\\SCLA_API_MQP\\AnotherFile-1", existing=True)
+file = FileParent("AnotherFile", "C:\\Users\\samka\\PycharmProjects\\SCLA_API_MQP\\AnotherFile", existing=False)
 
-# experiment_1 = file.add_experiment(name="Experiment1")
-# dataset_1 = experiment_1.create_dataset(name="random", size=(50000, 3000), datatype='float32')
-# dataset_1.add_data(data_to_add=random)
-#
-# experiment_2 = file.add_experiment(name="Experiment2")
-# dataset_2 = experiment_2.create_dataset(name="fixed", size=(50000, 3000), datatype='float32')
-# dataset_2.add_data(data_to_add=fixed)
+experiment_1 = file.add_experiment(name="Experiment1")
+dataset_1 = experiment_1.create_dataset(name="random", size=(50000, 3000), datatype='float32')
+dataset_1.add_data(data_to_add=random)
 
-print("")
+experiment_2 = file.add_experiment(name="Experiment2")
+dataset_2 = experiment_2.create_dataset(name="fixed", size=(50000, 3000), datatype='float32')
+dataset_2.add_data(data_to_add=fixed)
