d26cbcac1281fbeba593f0de537bc2381cfb6866
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2024-04-12 23:28:57 -0400

    benchmark file format with varying traces

--- Testing/FileFormatTesting.py
+++ Testing/FileFormatTesting.py
@@ -2,6 +2,8 @@ from Testing.MetricVerification import read_bin_file_traces
 from WPI_SCA_LIBRARY.FileFormat import *
 from WPI_SCA_LIBRARY.Metrics import *
 import h5py
+import time
+import matplotlib.pyplot as plt
 
 # fixed = read_bin_file_traces("unprotected_sbox\\single\\traces\\oscilloscope_traces\\oscilloscope_traces_50k_3000_samples_fixed_positive_uint8_t.bin")
 # random = read_bin_file_traces("unprotected_sbox\\single\\traces\\oscilloscope_traces\\oscilloscope_traces_50k_3000_samples_random_positive_uint8_t.bin")
@@ -39,3 +41,42 @@ import h5py
 #
 # file = FileParent("AnotherFile-2", "C:\\Users\\samka\\PycharmProjects\\SCLA_API_MQP\\", existing=True)
 # file.get_experiment("Experiment1").calculate_snr("traces", snr2_intermediate, "keys", "plaintexts", "rout", visualize=True, save_data=True, save_graph=True)
+
+
+def benchmark_file_format():
+    # fixed = read_bin_file_traces("unprotected_sbox\\single\\traces\\oscilloscope_traces\\oscilloscope_traces_50k_3000_samples_fixed_positive_uint8_t.bin")
+    # random = read_bin_file_traces("unprotected_sbox\\single\\traces\\oscilloscope_traces\\oscilloscope_traces_50k_3000_samples_random_positive_uint8_t.bin")
+    # traces_100k = np.concatenate((fixed, random))
+
+    num_traces = [1000, 5000, 10000, 15000, 20000, 25000, 30000, 40000, 50000, 75000, 80000, 100000]
+    times = []
+    h5_times = []
+
+    for num_trace in num_traces:
+        data = np.random.random(size=(num_trace, 3000))
+        # file framework
+        start = time.time()
+        file = FileParent(name="AnotherFile", path="C:\\Users\\samka\\OneDrive\\Desktop\\", existing=True)
+        exp = file.get_experiment("Experiment1")
+        exp.add_dataset("dataset", data, datatype="float64")
+        end = time.time()
+        times.append(end - start)
+
+        # hdf5
+        start = time.time()
+        with h5py.File("data.h5", "w") as file:
+            file.create_dataset("dataset", data=data)
+        end = time.time()
+        h5_times.append(end - start)
+        print("Done {}".format(num_trace))
+
+    plt.plot(num_traces, times, label="Custom File Framework")
+    plt.plot(num_traces, h5_times, label="HDF5")
+    plt.legend()
+    plt.ylabel("Time (s)")
+    plt.xlabel("Number of traces")
+    plt.title("File Framework Benchmark: Varying Traces")
+    plt.grid()
+    plt.show()
+
+benchmark_file_format()
