d21c0e447bc2c71cf77bf324abb2a37a51092ea4
Trye1243 <trye1243@gmail.com>
2024-04-16 01:21:38 -0400

    commented

--- Testing/Benchmarks/benchmarking_riun.py
+++ Testing/Benchmarks/benchmarking_riun.py
@@ -1,6 +1,6 @@
 from metric_benchmarking import *
-snr_benchmarking()
-t_test_benchmarking()
+#snr_benchmarking()
+#t_test_benchmarking()
 correlation_metric()
 score_and_rank_benchmarking()
 sucess_rate_guessing_entropy_benchmarking()
