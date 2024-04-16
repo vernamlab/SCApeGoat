8defffe1f2b2b20cecae13642c7bba3a0e36d0f6
Trye1243 <trye1243@gmail.com>
2024-04-15 20:58:31 -0400

    Added success rate guessing entropy benchmarking

--- Testing/Benchmarks/metric_benchmarking.py
+++ Testing/Benchmarks/metric_benchmarking.py
@@ -83,3 +83,32 @@ def score_and_rank_benchmarking():
             del texts
             benchmarking_results.append([traces, samples, end - start])
     print(benchmarking_results)
+
+def sucess_rate_guessing_entropy_benchmarking():
+    num_of_samples = [1000]
+    num_of_traces = [100, 1000, 10000, 20000, 30000, 50000, 100000]
+    num_of_experiments = [8, 16, 32]
+    key_candidates = range(256)
+    benchmarking_results = []
+    unmasked_random = np.random.random_sample((100000, 10))
+    texts = np.random.randint(16,size=(100000, 16))
+    print("entered")
+    ranks_unprotected = score_and_rank(key_candidates, 0, unmasked_random, score_with_correlation, texts, leakage_model_hamming_distance)
+    print("exit")
+
+    for traces in num_of_traces:
+        for exp in num_of_experiments:
+
+            experiment_keys = np.empty(exp)
+            experiment_ranks_unprotected = np.empty(exp, dtype=object)
+
+            for i in range(exp):
+                experiment_keys[i] = 203
+                experiment_ranks_unprotected[i] = ranks_unprotected[:traces]
+
+            start = time.time()
+            success_rate_guessing_entropy(experiment_keys, experiment_ranks_unprotected, 1, exp)
+            end = time.time()
+            print(end-start)
+            benchmarking_results.append([traces, exp, end-start])
+    print(benchmarking_results)
