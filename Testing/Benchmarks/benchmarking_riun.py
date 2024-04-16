7c630e76ce5a4c8b0c834c035d87e788f4960022
Trye1243 <trye1243@gmail.com>
2024-04-16 01:34:15 -0400

    Changes to benchmark run

--- Testing/Benchmarks/benchmarking_riun.py
+++ Testing/Benchmarks/benchmarking_riun.py
@@ -1,6 +1,7 @@
 from metric_benchmarking import *
 #snr_benchmarking()
 #t_test_benchmarking()
+print("started")
 correlation_metric()
 score_and_rank_benchmarking()
 sucess_rate_guessing_entropy_benchmarking()
